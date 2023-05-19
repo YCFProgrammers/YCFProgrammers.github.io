bc036d6ed8adba923630f4c5b43eb0eb344a9dd3
YCFProgrammers <132219265+YCFProgrammers@users.noreply.github.com>
2023-05-18 21:26:02 -0500

    new search

---
+++ assets/js/RandomSearch.js
@@ -0,0 +1,21 @@
+let suggestions = [
+    "Rio de Janeiro",
+    "San Pablo",
+    "Nueva York",
+    "YouTube",
+    "YouTuber",
+    "Miami",
+    "Blogger",
+    "Destino",
+    "Tienda",
+    "Carros",
+    "Chinampas",
+    "Freelancer",
+    "Facebook",
+    "Diseñador",
+    "Desarrollador",
+    "Diseñador web",
+    "Desarrollador web",
+    "Como leer codigo",
+    
+];
